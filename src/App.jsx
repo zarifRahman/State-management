24538a2387a2ab62c76a9a97b1ca58696c500a19
Zarif Shahriar <zarif.shahriar.rahman@gmail.com>
2022-01-17 02:09:01 +0600

    refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,17 @@
-import React, {useState, useEffect} from "react";
+import React, {} from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import Spinner from "./Spinner";
-import useFetch from "./services/useFetch";
+import Products from "./Products";
 
 export default function App() {
-  const [size, setSize] = useState("");
-  const { data:products,loading,error } = useFetch("products?category=shoes");
-
-  function renderProduct(p) {
-    return (
-      <div key={p.id} className="product">
-        <a href="/">
-          <img src={`/images/${p.image}`} alt={p.name} />
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-        </a>
-      </div>
-    );
-  }
-  const filteredProduct = size 
-    ? products.filter((product) => product.skus.find(skus => skus.size === parseInt(size))) 
-    : products;
-
-  // throw error before rendering jsx
-  if(error) throw error;
-
-  if(loading) return <Spinner />
 
   return (
     <>
       <div className="content">
         <Header />
         <main>
-          {/* filter section */}
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select 
-              id="size" 
-              value={size} 
-              onChange={e => setSize(e.target.value)}
-            >
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-          </section>
-          {/* if size is true then render " " is false */}
-          {size && <h2>Found {filteredProduct.length} items</h2>}
-
-          {/* product section */}
-          <h1>All Products</h1>
-          <section id="products">
-            {/* map automatically passes each product to the renderProduct function */}
-            {/* it is called "point free style" */}
-            {filteredProduct.map(renderProduct)}
-          </section>
+          <Products/>
         </main>
       </div>
       <Footer />
---
+++ src/Products.jsx
@@ -0,0 +1,57 @@
+import React, {useState} from "react";
+import Spinner from "./Spinner";
+import useFetch from "./services/useFetch";
+
+export default function Products() {
+  const [size, setSize] = useState("");
+  const { data:products,loading,error } = useFetch("products?category=shoes");
+
+  function renderProduct(p) {
+    return (
+      <div key={p.id} className="product">
+        <a href="/">
+          <img src={`/images/${p.image}`} alt={p.name} />
+          <h3>{p.name}</h3>
+          <p>${p.price}</p>
+        </a>
+      </div>
+    );
+  }
+  const filteredProduct = size 
+    ? products.filter((product) => product.skus.find(skus => skus.size === parseInt(size))) 
+    : products;
+
+  // throw error before rendering jsx
+  if(error) throw error;
+
+  if(loading) return <Spinner />
+
+  return (
+    <>
+      {/* filter section */}
+      <section id="filters">
+        <label htmlFor="size">Filter by Size:</label>{" "}
+        <select 
+          id="size" 
+          value={size} 
+          onChange={e => setSize(e.target.value)}
+        >
+          <option value="">All sizes</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+        </select>
+      </section>
+      {/* if size is true then render " " is false */}
+      {size && <h2>Found {filteredProduct.length} items</h2>}
+
+      {/* product section */}
+      <h1>All Products</h1>
+      <section id="products">
+        {/* map automatically passes each product to the renderProduct function */}
+        {/* it is called "point free style" */}
+        {filteredProduct.map(renderProduct)}
+      </section>
+    </>
+  );
+}
