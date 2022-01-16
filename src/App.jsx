c5b9009f3706a30f382112d1f14e261ccd1a3487
Zarif Shahriar <zarif.shahriar.rahman@gmail.com>
2022-01-17 01:26:48 +0600

    useFetch hooks for API

--- src/App.jsx
+++ src/App.jsx
@@ -2,69 +2,12 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import { getProducts } from "./services/productService";
 import Spinner from "./Spinner";
-
-const products = [
-  {
-    "id": 1,
-    "category": "shoes",
-    "image": "shoe1.jpg",
-    "name": "Hiker",
-    "price": 94.95,
-    "skus": [
-      { "sku": "17", "size": 7 },
-      { "sku": "18", "size": 8 }
-    ],
-    "description": "This rugged boot will get you up the mountain safely."
-  },
-  {
-    "id": 2,
-    "category": "shoes",
-    "image": "shoe2.jpg",
-    "name": "Climber",
-    "price": 78.99,
-    "skus": [
-      { "sku": "28", "size": 8 },
-      { "sku": "29", "size": 9 }
-    ],
-    "description": "Sure-footed traction in slippery conditions."
-  },
-  {
-    "id": 3,
-    "category": "shoes",
-    "image": "shoe3.jpg",
-    "name": "Explorer",
-    "price": 145.95,
-    "skus": [
-      { "sku": "37", "size": 7 },
-      { "sku": "38", "size": 8 },
-      { "sku": "39", "size": 9 }
-    ],
-    "description": "Look stylish while stomping in the mud."
-  }
-]
+import useFetch from "./services/useFetch";
 
 export default function App() {
   const [size, setSize] = useState("");
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-  
-  // implement async await
-  useEffect(() => {
-    async function init() {
-      try {
-        const response = await getProducts("shoes")
-        .then((response) => setProducts(response))
-      }catch(error) {
-        setError(error)
-      }finally {
-        setLoading(false);
-      }
-    }
-    init();
-  },[]);
+  const { data:products,loading,error } = useFetch("products?category=shoes");
 
   function renderProduct(p) {
     return (
