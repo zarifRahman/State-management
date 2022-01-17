9b9119e1ba2effb8684e47f30ae419ff6879d039
zarifRahman <zarif.shahriar.rahman@gmail.com>
2022-01-17 15:17:14 +0600

    Product fetch through id in detail page

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
+import PageNotFound from './PageNotFound';
+import useFetch from './services/useFetch';
+import Spinner from './Spinner';
+
 
 export default function Detail() {
+  const {id} = useParams();
+  const { data:product,loading,error } = useFetch("products/" + id);
+
+  if(error) throw error;
+  if(loading) return <Spinner />
+  if(product.length === 0) return <PageNotFound />
+
   return (
-    <div>
-      <h1>Details</h1>
+    <div id="detail">
+      <h1>{product?.name}</h1>
+      <p>${product?.price}</p>
+      <img src={`/images/${product?.image}`} alt={product?.name} />
     </div>
   )
 }
